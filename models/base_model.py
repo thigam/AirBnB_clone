2bc4c2e7ec308b508b6ae9d6adb41fc571dd8558
thigam <matthewthiga72@gmail.com>
2024-03-08 14:58:30 +0100

    push for Base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -5,28 +5,40 @@ A module containing the definition for the base class that defines all common at
 """
 import uuid
 from datetime import datetime
+from .__init__ import storage
 
 
 class BaseModel:
     """The base class that contains all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """The initialization method for new instances"""
-        self.id = str(uuid.uuid(4))
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key,value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    self.key = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.key = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """The custom __str__ called upon using the print() function"""
-        return f"[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """The save function that saves to a file"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """A function that customizes the output of __dict__ attribute"""
         custom_dictionary = {}
-        for key, value in elf.__dict__.items():
+        for key, value in self.__dict__.items():
             if value is None:
                 continue
             if key == "created_at" or key == "updated_at":
