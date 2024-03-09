f83dbb9a88b5681a40400349a262ec6e362d31b8
thigam <matthewthiga72@gmail.com>
2024-03-09 19:17:27 +0100

    Corrected pycodestyle errors

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,12 @@ import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
     """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-       
+
         """Initialization of  instance attributes
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
@@ -30,7 +31,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-    
+
     def __str__(self):
         """Returns the string representation"""
         return "[{}] ({}) {}".\
@@ -38,7 +39,7 @@ class BaseModel:
 
     def save(self):
         """updates the public instance attribute updated_at"""
-         self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
