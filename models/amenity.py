251c1317ff9910ad9fb9a2f3b075bbd9a8fed274
thigam <matthewthiga72@gmail.com>
2024-03-16 17:06:06 +0100

    Switching content from thiga to main branch

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Contains the definition for a class User that inherits from BaseModel"""
-from .base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """A class that holds information about the available amenities"""
-    name = ""
