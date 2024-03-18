463958f1abe5b94e9e52ab61e61594ada2bc2021
thigam <matthewthiga72@gmail.com>
2024-03-18 21:46:18 +0100

    Clearing

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Contains the definition for a class User that inherits from BaseModel"""
-from .base_model import BaseModel
-
-
-class City(BaseModel):
-    """Holds information about the individual's city"""
-    state_id = ""
-    name = ""
--- web_static/README.md
+++
@@ -1,1 +0,0 @@
-This is the begginig of front end part of Air BNB clone by the use of HTML/CSS and the module is called Webstatic.
